1f1a9aa4b258168cd95d267e84dd7cb02ba3609f
danielhart17 <43150766+danielhart17@users.noreply.github.com>
2019-02-22 18:10:35 -0500

    Function 1
    completed function 1 but still has ' ERROR: char_code/2: Cannot represent due to `character_code' ' when sum-up-numbers-simple(L, N) evaluates true

--- prolog.pl
+++ prolog.pl
@@ -1,1 +1,11 @@
-
+% Base Case for empty list
+sum-up-numbers-simple([], 0).
+% Recursive Case for when list is not empty
+sum-up-numbers-simple([First|Rest], Sum) :-
+number(First),
+sum-up-numbers-simple(Rest, Sum1),
+Sum is First + Sum1.
+% Recursive Case for when list is not empty and encounters non-number
+sum-up-numbers-simple([First|Rest], Sum1) :-
+\+(number(First)),
+sum-up-numbers-simple(Rest, Sum1).
