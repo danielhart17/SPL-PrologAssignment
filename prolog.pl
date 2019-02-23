14a4ef0baa71b4dff58995a19035dbde42603a61
danielhart17 <43150766+danielhart17@users.noreply.github.com>
2019-02-22 22:24:28 -0500

    predicate 3
    logic for predicate 3 - still needs bug fixes

--- prolog.pl
+++ prolog.pl
@@ -28,3 +28,26 @@ sum-up-numbers-general([FirstElem|Rest],Sum) :-
 sum-up-numbers-general([First|Rest],Sum) :-
     not(number(First)),
     sum-up-numbers-general(Rest, Sum).
+
+% 3. PREDICATE TO FIND THE MIN IN L1 BIGGER THAN THE MIN IN L2
+
+min-above-min(L1, L2, N) :-
+%Find the min in L2 with findMin 
+findMin(L2, Min2),
+include(findLarger(Min2), L1, MinL1),
+%Find the min in L1 larger than the min in L2
+findMin(MinL1, N).
+%Compare which values are larger than the other
+findLarger(Val, N) :- 
+N > Val.
+% Find the minimum of a list
+% Base Case if there is only one value in the list, it is the minimum
+findMin([First], First).
+% Recursive Case where the head is the minimum of the list
+findMin([First|Rest], First) :-
+findMin(Rest, Min1), 
+First < Min1.
+% Recursive Case where the calculated minimum is the minimum of the list
+findMin([First|Rest], Min1) :-
+findMin(Rest, Min1), 
+First >= Min1.
